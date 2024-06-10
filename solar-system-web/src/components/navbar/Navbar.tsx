8158913cff4d1ff09f029b080962ce90fa297c1a
LucaszLisboa <gabriel.lisboa.141@gmail.com>
2024-06-10 02:06:36 -0300

    ajustando navbar com react bootstrap

--- solar-system-web/src/components/navbar/Navbar.tsx
+++ solar-system-web/src/components/navbar/Navbar.tsx
@@ -1,5 +1,6 @@
 import { signOut, getAuth } from "firebase/auth";
 import { NavLink } from "react-router-dom";
+import { Container, Nav, Navbar as NavbarComponent, Button } from "react-bootstrap";
 import "./Navbar.css";
 
 interface NavbarProps {
@@ -22,19 +23,32 @@ export function Navbar({ user }: NavbarProps) {
   }
 
   return (
-    <nav className="navbar">
-      <ul className="links">
-        <li><NavLink to="/home" className={({ isActive, isPending }) => isPending ? "pending" : isActive ? "active" : ""}>Home</NavLink></li>
-        <li><NavLink to="/quizz">Quizz</NavLink></li>
-        <li><NavLink to="/login">Picture of Day NASA</NavLink></li>
-      </ul>
-      <ul className="links">
-        <li>Olá {user}</li>
-        <li>
-          <button onClick={() => { handleSignOut() }}> Sair </button>
-        </li>
-      </ul>
-    </nav>
+    <NavbarComponent bg="black" data-bs-theme="dark" expand="lg" className="bg-gradient">
+      <Container fluid>
+        <Nav className="flex-row gap-3">
+          <NavbarComponent.Brand as={NavLink} to="/home">
+            <img
+              src="src\assets\icons\planeta-terra.png"
+              width="30"
+              height="30"
+              className="d-inline-block align-top"
+              alt="Terra logo"
+            />
+          </NavbarComponent.Brand>
+          <Nav.Link as={NavLink} to="/home">Home</Nav.Link>
+          <Nav.Link as={NavLink} to="/quizz">Quizz</Nav.Link>
+          <Nav.Link as={NavLink} to="/login">Picture of Day NASA</Nav.Link>
+        </Nav>
+        <Nav className="d-flex align-items-center flex-row gap-3">
+          <Nav.Item>
+            <span>Olá {user}</span>
+          </Nav.Item>
+          <Nav.Item>
+            <Button variant="primary" onClick={handleSignOut}>Sair</Button>
+          </Nav.Item>
+        </Nav>
+      </Container>
+    </NavbarComponent>
   )
 }
 
